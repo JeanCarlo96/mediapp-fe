bc25b57926d71efc182e2491254933afd54dfe9f
Jean Marchesini <jeancarlo.marchesini@yahoo.es>
2024-12-19 23:31:52 -0500

    JCMA Angular Material

---
+++ angular-material/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, MatButtonModule, MatCardModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-material';
+}
